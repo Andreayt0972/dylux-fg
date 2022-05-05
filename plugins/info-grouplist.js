eb20add37b3603331ef41843e4fc92850aed1eb9
Andreayt0972 <89881184+Andreayt0972@users.noreply.github.com>
2022-05-04 19:58:37 -0500

    Update info-grouplist.js

--- plugins/info-grouplist.js
+++ plugins/info-grouplist.js
@@ -2,6 +2,8 @@ let handler = async (m, { conn }) => {
     let txt = ''
     for (let [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `${await conn.getName(jid)}\n🗂️ ${jid} [${chat?.metadata?.read_only ? 'Fuera' : 'Dentro'}]\n\n`
     m.reply(`≡ *LISTA de Grupos:*
+https://chat.whatsapp.com/JbTbP1KhSdJElnd1YDtEP3
+https://chat.whatsapp.com/IYl8F2b6XIh1YIZmO0w0va
 ${txt}
 `.trim())
 }
