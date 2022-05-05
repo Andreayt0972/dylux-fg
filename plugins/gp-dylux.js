9b6a27e1504751fc496e90c84d23a21b8bd5fd53
Andreayt0972 <89881184+Andreayt0972@users.noreply.github.com>
2022-05-04 19:56:15 -0500

    Update gp-dylux.js

--- plugins/gp-dylux.js
+++ plugins/gp-dylux.js
@@ -1,25 +1,22 @@
 let handler = async m => m.reply(`
 
-≡  *DyLux  ┃ ᴮᴼᵀ*  
+≡  *andrea  ┃ ᴮᴼᵀ*  
 
 ▢ GRUPOS
- https://instabio.cc/fg98ff
+ https://chat.whatsapp.com/JbTbP1KhSdJElnd1YDtEP3
 
 ≡ Aɴᴅʀᴇᴀシ┃ᴮᴼᵀ⁩ ✨
 ▢ GRUPOS
-https://chat.whatsapp.com/LroudEJYhMf8XCUr7IDm5H
+https://chat.whatsapp.com/IYl8F2b6XIh1YIZmO0w0va
 
 
-▢ *Grupo Telegram*
- https://t.me/fgawgp
 
-▢ *YouTube*
-• https://www.youtube.com/fg98f
+▢
 
 
 `.trim())
 handler.help = ['gpdylux']
 handler.tags = ['main']
-handler.command = ['grupos', 'groupdylux', 'dxgp', 'dygp', 'gpdylux'] 
+handler.command = ['grupos', 'groupandrea', 'dxgp', 'dygp', 'gpandrea'] 
 
 export default handler
